b84ca86eb54fcd4586712fdb85592777a360e79d
JiaLee <s2018s37@e-mirim.hs.kr>
2022-12-31 20:37:41 +0900

    pan bug fix && text Edit

--- App.js
+++ App.js
@@ -8,184 +8,171 @@
 
 import React, {useEffect, useRef, useState} from 'react';
 import type {Node} from 'react';
+
 import {
-  Button,
-  Image,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet, Text,
-  useColorScheme, View, TextInput
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    useColorScheme,
 } from 'react-native';
 
 import {
-  Colors,
+    Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
 import PermissionsConfig from "./src/config/PermissionsConfig";
 import {launchImageLibrary} from "react-native-image-picker";
 import ViewShot from "react-native-view-shot";
-import { CameraRoll } from "@react-native-camera-roll/camera-roll";
+import {CameraRoll} from "@react-native-camera-roll/camera-roll";
 
 import {
-  Gesture,
-  GestureHandlerRootView,
-  GestureDetector
+    Gesture,
+    GestureHandlerRootView,
+    GestureDetector,
 } from "react-native-gesture-handler";
 import Animated, {
-  useAnimatedGestureHandler,
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
+    useAnimatedStyle, useDerivedValue,
+    useSharedValue,
 } from 'react-native-reanimated';
 import {Btn, Img} from "./src/view/styled/TestStyled";
+import DragTextEditorPresenter from "./src/view/presenter/DragTextEditorPresenter";
 
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const [photo, setPhoto] = useState(null);
-
-  const viewShotRef = useRef(null);
-
-  const context = useSharedValue({x: 0, y: 0});
-  const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
-  const scale = useSharedValue(1);
-  // const width = useSharedValue(100);
-
-  const panGesture = Gesture.Pan()
-      .onBegin((event) => {
-        context.value = {x: translateX.value, y: translateY.value};
-      })
-      .onUpdate((event) => {
-        translateX.value = event.translationX + context.value.x;
-        translateY.value = event.translationY + context.value.y;
-      });
-
-  panGesture.enableTrackpadTwoFingerGesture(true)
-
-  const pinchGesture = Gesture.Pinch()
-      // .onBegin(e => {
-      //   // runOnJS(props.onPinchImage)(props.postActivityId)
-      // })
-      .onUpdate((event) => {
-        scale.value = event.scale
-      })
-      // .onEnd(() => {
-      //   scale.value = withTiming(1)
-      // });
-
-  const composed = Gesture.Simultaneous(panGesture, pinchGesture);
-
-  // When shared value changes. animated style update the values accordingly that.
-  const rStyle = useAnimatedStyle(() => {
-    return {
-      width: width?.value,
-      transform: [
-        {translateX: translateX?.value},
-        {translateY: translateY?.value},
-        {scale: scale?.value}
-      ],
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
     };
-  });
 
-  useEffect(() => {
-    PermissionsConfig();
-  }, []);
+    const [photo, setPhoto] = useState(null);
 
-  const showPicker = async() =>{
-    const result = await launchImageLibrary();
-    if (result.didCancel){
-      return null;
-    }
-    console.log('result', result.didCancel);
-    const localUri = result.assets[0].uri;
-    const uriPath = localUri.split("//").pop();
-    const imageName = localUri.split("/").pop();
-    setPhoto("file://"+uriPath);
-  }
+    const viewShotRef = useRef(null);
 
-  const save = () => {
-    viewShotRef.current.capture().then((url) => {
-      console.log('url', url);
-      // CameraRoll.saveToCameraRoll(url).then(r => {
-      //   console.log('🐤result', r);
-      // });
-      CameraRoll.save(url, "photo").then(r => {
-        console.log('🐤result', r);
-      });
+    const context = useSharedValue({x: 0, y: 0});
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+    const scale = useSharedValue(1);
+
+    const panGesture = Gesture.Pan()
+        .onFinalize((event) => console.log('onFinalize'))
+        .onBegin((event) => {
+            console.log('onStart', event);
+            context.value = {x: translateX.value, y: translateY.value};
+            // if (event.state === 2) State.ACTIVE();
+        })
+        .onUpdate((event) => {
+            console.log('onUpdate', event);
+            translateX.value = event.translationX + context.value.x;
+            translateY.value = event.translationY + context.value.y;
+        });
+
+    panGesture.enableTrackpadTwoFingerGesture(true)
+
+    const pinchGesture = Gesture.Pinch()
+        .onUpdate((event) => {
+            console.log('onUpdate Pinch', event);
+            scale.value = event.scale
+        });
+
+    const composed = Gesture.Simultaneous(panGesture, pinchGesture);
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {translateX: translateX?.value},
+                {translateY: translateY?.value},
+                {scale: scale?.value}
+            ],
+        };
     });
-  }
 
-  return (
-    <SafeAreaView style={[backgroundStyle, styles.Screen]}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <GestureHandlerRootView style={styles.Screen}>
-        <GestureDetector gesture={composed}>
-          <ViewShot ref={viewShotRef} style={{position: 'relative', flex: 2, width: '100%',
-            // alignItems: "center",
-            justifyContent: "center",
-            backgroundColor: '#000000'}}>
-            <Img source={{uri:photo}} resizeMode="contain" />
-            <Animated.View
-                style={[styles.square, rStyle]}
-            >
-              <TextInput style={{ height: 50, width: 50, backgroundColor: 'white' }} />
-            </Animated.View>
-          </ViewShot>
-        </GestureDetector>
-        <Btn title="이미지 선택" onPress={showPicker}></Btn>
-        <Btn title="텍스트 추가" onPress={showPicker}></Btn>
-        <Btn title="저장?" onPress={save}></Btn>
-      </GestureHandlerRootView>
-    </SafeAreaView>
-  );
+    useEffect(() => {
+        PermissionsConfig();
+    }, []);
+
+    const showPicker = async () => {
+        const result = await launchImageLibrary();
+        if (result.didCancel) return null;
+
+        console.log('result', result.didCancel);
+        const localUri = result.assets[0].uri;
+        const uriPath = localUri.split("//").pop();
+        // const imageName = localUri.split("/").pop();
+        setPhoto("file://" + uriPath);
+    }
+
+    const save = () => {
+        viewShotRef.current.capture().then((url) => {
+            console.log('url', url);
+            CameraRoll.save(url, "photo").then(r => {
+                console.log('🐤result', r);
+            });
+        });
+    }
+
+    return (
+        <SafeAreaView style={[backgroundStyle, styles.Screen]}>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                backgroundColor={backgroundStyle.backgroundColor}
+            />
+            <GestureHandlerRootView style={styles.Screen}>
+                <GestureDetector gesture={composed}>
+                    <ViewShot ref={viewShotRef} style={{
+                        position: 'relative', flex: 2, width: '100%',
+                        alignItems: "center",
+                        justifyContent: "center",
+                        // backgroundColor: '#000000'
+                    }}>
+                        <Img source={{uri: photo}} resizeMode="contain"/>
+                        <DragTextEditorPresenter animatedStyle={animatedStyle} />
+                    </ViewShot>
+                </GestureDetector>
+                <Btn title="이미지 선택" onPress={showPicker}></Btn>
+                <Btn title="텍스트 추가" onPress={showPicker}></Btn>
+                <Btn title="저장?" onPress={save}></Btn>
+            </GestureHandlerRootView>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  Screen: {
-    // display: "flex",
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    alignItems: "center",
-    justifyContent: "center",
-    // backgroundColor: "#000000",
-  },
-  img: {
-    width: '100%',
-    height: '100%',
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  dropzone: {
-    backgroundColor: 'rgb(153,187,255)',
-  },
-  square: {
-    borderRadius: 15,
-    backgroundColor: "#99BBFFFF",
-  },
+    Screen: {
+        display: "flex",
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: "center",
+        justifyContent: "center",
+        // backgroundColor: "#000000",
+    },
+    img: {
+        width: '100%',
+        height: '100%',
+    },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
+    dropzone: {
+        backgroundColor: 'rgb(153,187,255)',
+    },
+    square: {
+        borderRadius: 15,
+        backgroundColor: "#99BBFFFF",
+    },
 });
 
 export default App;
--- src/view/presenter/DragTextEditorPresenter.js
+++ src/view/presenter/DragTextEditorPresenter.js
@@ -1,20 +1,36 @@
-import React from "react";
-import {TextInput} from "react-native";
+import React, {useState} from "react";
 
 import Animated from "react-native-reanimated";
+import {TextInput} from "react-native-gesture-handler";
 
-const DragTextEditorPresenter = () => {
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+const DragTextEditorPresenter = ({animatedStyle}) => {
+    const [text, setText] = useState('');
+
     return (
-        <Animated.View
-            // style={[styles.square, { height: 100, width: 100 }, rStyle]}
-        >
-            <TextInput style={{ height: 50, width: 50, backgroundColor: 'white' }} />
-        </Animated.View>
+            <AnimatedTextInput
+                multiline={true}
+                style={
+                    [
+                        animatedStyle,
+                        {
+                            borderColor: '#000000',
+                            borderWidth: 1,
+                            color: '#000000',
+                            position: 'absolute',
+                            minWidth: '10%',
+                            // minHeight: '10%',
+                        }
+                    ]
+                }
+                value={text}
+                onChangeText={(text) => setText(text)}
+            />
     );
 }
 
-const styles = StyleSheet.create({
-
-});
+// const styles = StyleSheet.create({
+//
+// });
 
 export default DragTextEditorPresenter;
